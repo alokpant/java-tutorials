4483c109e65381bfcbca92450630987ecf3b2ada
mindl3ss <justtodesign@gmail.com>
2024-08-10 20:05:30 +0200

    fix: add getInputFromScanner

--- src/ReadingUserInput.java
+++ src/ReadingUserInput.java
@@ -1,11 +1,17 @@
 import java.util.Calendar;
+import java.util.Scanner;
 
 import static java.lang.Integer.parseInt;
 
 public class ReadingUserInput {
     public static void main(String[] args) {
         int currentYear = Calendar.getInstance().get(Calendar.YEAR);
-        int userInput = getInputFromConsole();
+        int userInput;
+        try {
+            userInput = getInputFromConsole();
+        } catch (NullPointerException e) {
+            userInput = getInputFromScanner(currentYear);
+        }
 
         System.out.println(currentYear);
         System.out.println(userInput);
@@ -15,5 +21,33 @@ public class ReadingUserInput {
     public static int getInputFromConsole() {
         String year = System.console().readLine("What year were you born?");
         return parseInt(year);
+    }
+
+    public static int getInputFromScanner(int currentYear) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("What year were you born?");
+        boolean validDOB = false;
+        do {
+            String year = scanner.nextLine();
+            int numberYear = parseInt(year);
+
+            if (checkData(currentYear, numberYear)) {
+                validDOB = true;
+                return parseInt(year);
+            }
+            System.out.println("The value that you entered is not valid: " + year + " \n Please try again");
+        } while (!validDOB);
+
+        return 0;
+    }
+
+    public static boolean checkData(int currentYear, int dateOfBirth) {
+        int minimumYear = currentYear - 125;
+
+        if (dateOfBirth < minimumYear || dateOfBirth > currentYear) {
+            return false;
+        }
+
+        return true;
     }
 }
